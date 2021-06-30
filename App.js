5fd791e264a2f54d76f1e17ba7e244f59cb6f757
KennetAvila28 <83607313+KennetAvila28@users.noreply.github.com>
2021-06-29 21:55:10 -0500

    first commit
    create screens

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+import CreateScreen from "./screens/CreateScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+import EditScreen from "./screens/EditScreen";
+import ListScreen from "./screens/ListScreen";
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="ListScreen" component={ListScreen} />
+      <Stack.Screen name="CreateScreen" component={CreateScreen} />
+      <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+      <Stack.Screen name="EditScreen" component={EditScreen} />
+    </Stack.Navigator>
+  );
+}
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
---
+++ screens/EditScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const EditScreen = () =>{
+    return(
+        <View>
+            <Text></Text>
+        </View>
+    );
+}
+export default EditScreen
